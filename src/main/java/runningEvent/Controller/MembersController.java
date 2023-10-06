7cc956b8bbbeae2eb19830f5f3430a1266109017
quagh03 <03.quanghuy.le@gmail.com>
2023-10-06 12:36:26 +0700

    getMemberById, catch exception

--- src/main/java/runningEvent/Controller/MembersController.java
+++ src/main/java/runningEvent/Controller/MembersController.java
@@ -1,12 +1,16 @@
 package runningEvent.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import runningEvent.Model.Members;
 import runningEvent.Repository.MembersRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MembersController {
@@ -19,7 +23,20 @@ public class MembersController {
 
     @GetMapping("/members")
     public List<Members> getAllMembers(){
-
         return membersRepository.findAll();
+    }
+
+    @GetMapping("/public/members/{id}")
+    public ResponseEntity<?> getMemberById(@PathVariable Integer id){
+        try{
+            Optional<Members> member = membersRepository.findById(id);
+            if(member.isPresent()){
+                return ResponseEntity.ok(member.get());
+            }else{
+                return ResponseEntity.notFound().build();
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>("Error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
